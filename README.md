e3c2fafee5b8c5c0f624622d4a9cc5930eeb9bb6
alexkalen <alexkalen96@gmail.com>
2022-05-24 11:33:44 -0400

    Creating Lottery Project

---
+++ README.md
@@ -0,0 +1,9 @@
+1. User can enter the loterry with ETH based on a USD fee.
+2. An admin will choose when an admin is over.
+3. The lottery will select a random winner
+
+How to test?
+
+1. `mainnet-fork`
+2. `development` with mocks
+3. `testnet`
---
+++ contracts/Lottery.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Lottery {
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+    }
+
+    function enter() public payable {
+        //50$ minimum
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "not enough ETH");
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+
+        uint256 adjustedPrice = uint256(price) * 10**10; // 18 decimals
+
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+
+        return costToEnter;
+    }
+
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "A lottery is already on its way! Cannot start lottery"
+        );
+
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public {}
+}
